f3bb1e574e99d3f74dac1147713e6f521afbc05f
Adryvals <adrvs94@gmail.com>
2026-04-19 22:17:18 -0400

    First Commit

---
+++ app/api/data.ts
@@ -0,0 +1,17 @@
+export const books = [
+  {
+    id: 1,
+    title: "The Great Gatsby",
+    description: "A novel by F. Scott Fitzgerald about the American dream and the Jazz Age.",
+  },
+  {
+    id: 2,
+    title: "To Kill a Mockingbird",
+    description: "A novel by Harper Lee that explores themes of racial injustice and moral growth in the American South.",
+  },
+  {
+    id: 78,
+    title: "1984",
+    description: "A dystopian novel by George Orwell that delves into themes of totalitarianism, surveillance, and individuality.",
+  }
+]
